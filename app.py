bdd461d76644780b060c43839ae02cb5ca17184e
Aditya T <aditsrivats@gmail.com>
2020-12-08 13:27:37 -0500

    adding login to my files

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, HiddenField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 import os
-
 
 
 app = Flask(__name__)
@@ -21,6 +23,7 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+login = LoginManager(app)
 
 
 class Pets(db.Model):
@@ -30,12 +33,101 @@ class Pets(db.Model):
     filename = db.Column(db.String(200), nullable=False)
     def __repr__(self):
         return 'Pet %r' % self.id
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    admin = db.Column(db.Boolean, default=False)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def set_admin(self):
+        self.admin = True
+
+    def __repr__(self):
+        return 'User %r ' % self.username
+
+
+##Forms
 class petForm(FlaskForm):
     name= StringField('Enter a new name')
     species = StringField('Enter a new species')
-    # picture = FileField('Upload image')
     submit = SubmitField('Submit new pet')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+
+
+##Paths
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Register', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -70,7 +162,7 @@ def upload_file():
             species = request.form['species']
             name = request.form['petname']
             filename = uploaded_file.filename
-            new_pet = Pets(species=species, filename=filename,name=name)
+            new_pet = Pets(species=species, age=age, filename=filename,name=name)
             try:
                 db.session.add(new_pet)
                 db.session.commit()
@@ -80,42 +172,15 @@ def upload_file():
                 print(name)
                 print(filename)
                 return "error"
-        # form.species.data = ''
-        # form.name.data = ''
-        # form.filename.data = ''
     pets = Pets.query.order_by(Pets.id)
     return redirect(url_for('index'))
-
-
-# def submit():
-#     pet = None
-#     form = petForm()
-#     if form.validate_on_submit():
-#         species = form.species.data
-#         picture = picture.save(form.picture.data)
-#         filename = picture.url(filename)
-#         saved_picture.save(os.path.join(app.config['UPLOAD_PATH'], picture)) 
-#         name = form.name.data
-#         new_pet = Pets(species=species, filename=filename,name=name)
-#         try:
-#             db.session.add(new_pet)
-#             db.session.commit()
-#             return redirect('/petlist')
-#         except:
-#             return "error"
-#         form.species.data = ''
-#         form.name.data = ''
-#         form.picture.data = ''
-#     pets = Pets.query.order_by(Pets.id)
-#     return render_template('addPet.html', form=form,pets=pets)
-
 
 @app.route('/petlist')
 def petlist():
     pets = Pets.query.order_by(Pets.id)
     return render_template('petList.html', pets=pets)
     
-@app.route('/petview')
+@app.route('/petview', methods=['POST', 'GET'])
 def viewpet():
     pet_id = request.args.get('id')
     pets = Pets.query.filter(Pets.id == pet_id)
