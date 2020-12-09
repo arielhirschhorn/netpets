023f60d2c4fdd4fdefc25336a65d39231c2427af
Katie Karabetsos <35354277+Katiejk@users.noreply.github.com>
2020-12-09 07:44:40 -0500

    Search Function now works
    Database has some non-square pets now... 
    Also switched sex to be a drop-down instead of check marks

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, HiddenField, BooleanField, PasswordField, validators, ValidationError
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from datetime import date
@@ -22,6 +25,7 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+login = LoginManager(app)
 
 
 class Pets(db.Model):
@@ -48,6 +52,100 @@ class petForm(FlaskForm):
     # picture = FileField('Upload image')
     submit = SubmitField('Submit new pet')
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    admin = db.Column(db.Boolean, default=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def set_admin(self):
+        self.admin = True
+
+    def __repr__(self):
+        return 'User %r ' % self.username
+
+
+##Forms
+#class petForm(FlaskForm):
+#    name= StringField('Enter a new name')
+#    species = StringField('Enter a new species')
+#    submit = SubmitField('Submit new pet')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+##Paths
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html', title='Register', form=form)
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -64,7 +162,8 @@ def index():
     #and then trimming it, but I just needed to get something working so I could work on styling it
     pets = Pets.query.order_by(Pets.id)
     previews = pets[:4]
-    return render_template('index.html', previews=previews)
+    petcount = Pets.query.count()
+    return render_template('index.html', previews=previews, petcount=petcount)
     
 @app.route('/addPet')
 def petList():
@@ -163,7 +262,6 @@ def upload_file():
     pets = Pets.query.order_by(Pets.id)
     return redirect(url_for('index'))
 
-
 # def submit():
 #     pet = None
 #     form = petForm()
@@ -191,9 +289,58 @@ def upload_file():
 def petlist():
     pets = Pets.query.order_by(Pets.id)
     return render_template('petList.html', pets=pets)
+
+@app.route('/petlist', methods=['GET','POST'])
+def search_pets():
+    species = request.form['species']
+    age = request.form['age']
+    sex = request.form['sex']
+    children = request.form.get('child')
+    otherPet = request.form.get('other-pet')
+    pets = Pets.query.order_by(Pets.id)
+    
+    if species == "dog":
+        pets = pets.filter(Pets.species==0)
+    elif species == "cat":
+        pets = pets.filter(Pets.species==1)
+    elif species == "small":
+        pets = pets.filter(Pets.species==2)
+    elif species == "bird":
+        pets = pets.filter(Pets.species==3)
+    elif species == "reptile":
+        pets = pets.filter(Pets.species==4)
+    elif species == "fish":
+        pets = pets.filter(Pets.species==5)
+    elif species == "fish":
+        pets = pets.filter(Pets.species==6)
+
+    if age == "baby":
+        pets = pets.filter(Pets.age==0)
+    elif age == "young":
+        pets = pets.filter(Pets.age==1)
+    elif age == "adult":
+        pets = pets.filter(Pets.age==2)
+    elif age == "elder":
+        pets = pets.filter(Pets.age==3)
+
+    if sex == "m":
+        pets = pets.filter(Pets.sex==0)
+    if sex == "f":
+        pets = pets.filter(Pets.sex==1)
+
+    if children == "on":
+        pets = pets.filter(Pets.kidFriendly==True)
+    if otherPet == "on":
+        pets = pets.filter(Pets.petFriendly==True)
+
+    return render_template('petList.html', pets=pets)
     
 @app.route('/petview')
 def viewpet():
     pet_id = request.args.get('id')
     pets = Pets.query.filter(Pets.id == pet_id)
     return render_template('petview.html', pet=pets[0])
+
+@app.route('/adoptdog')
+def adoptdog():
+   return render_template('adoptdog.html')
